41363c90244a573c85c8bbc61dec22529d9f9812
Jhamky <jhammanueltablet@gmail.com>
2023-08-09 14:57:52 -0600

    Menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/config/router/app_router.dart';
-import 'package:flutter_application_1/config/theme/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,7 +13,10 @@ class MainApp extends StatelessWidget {
     return MaterialApp.router(
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: Apptheme().getTheme(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.pink,
+      ),
     );
   }
 }
--- lib/presentacion/screens/home_screen.dart
+++ lib/presentacion/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/presentacion/shared/customNavagationBar.dart';
+import 'package:flutter_application_1/menu/Menu.dart';
 import 'package:go_router/go_router.dart';
 
 class HomeView extends StatefulWidget {
@@ -11,19 +11,54 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
   int selectState = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          child: const Text('Vamos a la otra pantalla'),
-          onPressed: () {
-            context.push('/favoriteview');
-          },
+        appBar: AppBar(
+          title: const Text('Menu'),
         ),
-      ),
+        body: const _HomeView());
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+
+        return _CustomListTile(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: () {
+        context.push(menuItem.link);
+      },
     );
   }
 }
