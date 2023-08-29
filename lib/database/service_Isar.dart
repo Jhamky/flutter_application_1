aac178f7a62598f507b6070419e5db6e832589c5
Jhamky <jhammanueltablet@gmail.com>
2023-08-28 19:12:51 -0600

    Imp. Isar

--- lib/database/service_Isar.dart
+++ lib/database/service_Isar.dart
@@ -1,17 +1,16 @@
-//import 'package:isar/isar.dart';
-//import 'package:path_provider/path_provider.dart';
-//import 'package:flutter_application_1/presentacion/models/Dirreciones_Isar.dart';
+import 'package:isar/isar.dart';
+import 'package:flutter_application_1/presentacion/models/Isardc.dart';
 
-//class IsarService {
-  //late Future<Isar> db;
-  //sarService() {
-    //db = openDb();
-  //}
+class IsarService {
+  late Future<Isar> db;
+  sarService() {
+    db = openDb();
+  }
 
-  //Future<Isar> openDb() async {
-    //if (Isar.instanceNames.isEmpty) {
-      //return await Isar.open([DirrecionesIsarSchema]);
-    //}
-    //return await Future.value(Isar.getInstance());
-  //}
-//}
+  Future<Isar> openDb() async {
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open([IsardcSchema]);
+    }
+    return await Future.value(Isar.getInstance());
+  }
+}
---
+++ lib/presentacion/models/Isardc.dart
@@ -0,0 +1,15 @@
+import 'package:isar/isar.dart';
+//import 'package:flutter_application_1/presentacion/screens/create_grup.dart';
+part 'Isardc.g.dart';
+
+@collection
+class Isardc {
+  Id id = Isar.autoIncrement;
+  String name = '';
+  String calle = '';
+  String codigopostal = '';
+  String estado = '';
+  String municipio = '';
+  String asentamiento = '';
+  String adicional = '';
+}
