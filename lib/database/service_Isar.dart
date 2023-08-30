ffeb3a7037374a57b08ca89f528e0664a678cc61
Jhamky <jhammanueltablet@gmail.com>
2023-08-30 14:42:58 -0600

    Isar

--- lib/database/service_Isar.dart
+++ lib/database/service_Isar.dart
@@ -3,13 +3,19 @@ import 'package:flutter_application_1/presentacion/models/Isardc.dart';
 
 class IsarService {
   late Future<Isar> db;
-  sarService() {
+  isarService() {
     db = openDb();
+  }
+
+  Future<List<Isardc>> saveDicc(Isardc newIsardc) async {
+    final isar = await db;
+    isar.writeTxn<String>(() => isar.newIsardc.putSync(newIsardc));
+    throw UnimplementedError();
   }
 
   Future<Isar> openDb() async {
     if (Isar.instanceNames.isEmpty) {
-      return await Isar.open([IsardcSchema]);
+      return await Isar.open([IsardcSchema], directory: '.');
     }
     return await Future.value(Isar.getInstance());
   }
